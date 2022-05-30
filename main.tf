f42a3c973a1abf28f4ff1c69bcc31460b78214e6
sivasgit <sivaprasad.r81@gmail.com>
2022-05-30 23:39:55 +0000

    tst

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "acg-terraform-cicd1245"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
